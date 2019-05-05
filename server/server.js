31a686a62f56a84a9fabaabd2f92228664962975
maheshraj.mainali@gmail.com <maheshraj.mainali@gmail.com>
2019-05-05 11:45:33 -0400

    heroku deployment changes

--- server/server.js
+++ server/server.js
@@ -51,9 +51,10 @@ if (isDev) {
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.resolve(__dirname, '../dist')));
 } else {
+  console.log(__dirname);
   app.use(express.static(path.resolve(__dirname, '../dist')));
   app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist','index.html'));
     res.end();
   });
 }
